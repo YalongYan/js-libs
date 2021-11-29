8cb3e7c656b32e7532dc2182129580b6d2368631
yanyalong <yanyalong@yidian-inc.com>
2021-11-29 09:38:57 +0800

    feat: init program

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+// 类型
+export { default as type } from './type/type';
+export { default as isEmpty } from './type/isEmpty';
+export { default as isJsonString } from './type/isJsonString';
+
+// 缓存
+export { default as cookie } from './cache/cookie';
+
+// 浏览器
+export { default as createLink } from './browser/createLink';
+export { default as createScript } from './browser/createScript';
+export { default as scrollToTop } from './browser/scrollToTop';
+export { default as urlGet } from './browser/urlGet';
+export { default as urlParams } from './browser/urlParams';
+
+// 时间
+export { default as sleep } from './time/sleep';
+export { default as timestamp } from './time/timestamp';
+export { default as timeBeauty } from './time/timeBeauty';
+export { default as getDay } from './time/getDay';
+export { default as getMonth } from './time/getMonth';
+export { default as getYear } from './time/getYear';
+
+// 函数式
+export { default as curry } from './func/curry';
+export { default as compose } from './func/compose';
+export { default as pipe } from './func/pipe';
+
+// 数据处理
+export { default as replace } from './data/replace';
+export { default as clone } from './data/clone';
+export { default as deepClone } from './data/deepClone';
+export { default as json2FormData } from './data/json2FormData';
+export { default as base64 } from './data/base64';
+export { default as pick } from './data/pick';
+
+// 数组
+export { default as arrayUnique } from './array/arrayUnique';
+
+// 验证
+export { default as isAndroid } from './is/isAndroid';
+export { default as isApple } from './is/isApple';
+export { default as isIos } from './is/isIos';
+export { default as isPhone } from './is/isPhone';
+export { default as isEmail } from './is/isEmail';
+export { default as isMobile } from './is/isMobile';
+export { default as isIP } from './is/isIP';
+export { default as isIDCard } from './is/isIDCard';
+export { default as isPostCode } from './is/isPostCode';
+export { default as isQQNumber } from './is/isQQNumber';
---
+++ src/type/isEmpty.ts
@@ -0,0 +1,28 @@
+import type from './type';
+
+/**
+ * 判断空对象，空数组，空字符串
+ * @param obj 数组或者对象或者字符串
+ * @returns boolean
+ */
+export default function isEmpty(obj: Array<any> | Object | string): boolean {
+  if (!obj) {
+    return true;
+  }
+  if(obj === ''){
+    return true;
+  }
+  if (type(obj) === 'array') {
+    // @ts-ignore
+    if (!obj.length) {
+      return true;
+    }
+  }
+  if (type(obj) === 'object') {
+    if (JSON.stringify(obj) === "{}") {
+      return true;
+    }
+  }
+  console.log([].includes('aa'))
+  return false;
+}
